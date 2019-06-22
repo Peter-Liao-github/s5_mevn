def3eb59dcefe7a3f1c93269f4cfe34a3953eebf
PiiterLiao <pittpeterliao@gmail.com>
2019-06-22 21:55:15 +0800

    init set, add loader pug & sass, normalize css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang='pug'>
+  #app
+    img(alt="Vue logo" src="./assets/logo.png")
+    HomeComponent(msg="Here's HomeComponent")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/HomeComponent.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HomeComponent
   }
 }
 </script>
 
 <style>
+@import url(http://fonts.googleapis.com/earlyaccess/notosanstc.css);
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans TC', \5FAE\8EDF\6B63\9ED1\9AD4, 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
